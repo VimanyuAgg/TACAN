b7fec9d9a88961672116e36476b892ee4864e463
VimanyuAgg <vimanyu1022007@gmail.com>
2017-12-02 17:37:48 -0800

    Fixed weightMatrix and Added unittests
    invalid tuple in weightMatrix, created new weightMatrix and added testNode() to check integration with weighMatrix

--- phase1/Node.py
+++ phase1/Node.py
@@ -4,12 +4,12 @@ class Node:
 	'''
 	Node class representing each node in the network
 	'''
-	def __init__(self,myId,parentid, childList,dist, clusterheadid,isClusterhead,state):
+	def __init__(self,myId,parentId, childListId,dist, clusterheadId,subtreeList,neighbourList,isClusterhead,state):
 		self.id = myId
 		self.parentId = parentId
 		self.childListId = childListId
 		self.dist = dist
-		self.clusterheadId = clusterheadid
+		self.clusterheadId = clusterheadId
 		self.subtreeList = subtreeList
 		self.neighbourList = neighbourList
 		self.weight = weightMatrix.getWeight(self.id)
@@ -17,8 +17,26 @@ class Node:
 		self.isClusterhead = isClusterhead
 		self.state = state
 
-	def getChildWeight():
+	def getChildWeight(self):
 		childWeight = 0
-		for c in childListId:
+		for c in self.childListId:
 			childWeight += weightMatrix.getWeight(c)
 		return childWeight
+
+def testNode():
+	n0 = Node
+	n1 = Node(1,0,[4],1,0,[4],[2,3],False,"active")
+	# print n1.parentId
+	assert n1.parentId == 0
+	print "Test 1 Passed"
+
+	assert n1.weight == 176
+	print "Test 2 Passed"
+
+	assert n1.neighbourList == [2,3]
+	print "Test 3 Passed - Integration with weightMatrix achieved"
+
+
+testNode()
+
+
