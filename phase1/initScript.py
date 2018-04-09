ec102191e78ba85c5bbb3c0d12a20328c6c6902e
VimanyuAgg <vimanyu1022007@gmail.com>
2018-04-08 19:17:51 -0700

    adding initscript with threading to spawn all nodes

--- phase1/initScript.py
+++ phase1/initScript.py
@@ -1,12 +1,37 @@
 from Node import Node
 from threading import Thread
+import time
 
 def spawnNode(id):
 	return Node(id)
 
 if __name__ == '__main__':
-	thread1 = Thread(target=spawnNode(),args=(1,))
-	thread2 = Thread(target=spawnNode(), args=(2,))
-	node2 = Node(2)
+	thread0 = Thread(target=spawnNode, args=('0',))
+	thread1 = Thread(target=spawnNode,args=('1',))
+	thread2 = Thread(target=spawnNode, args=('2',))
+	thread3 = Thread(target=spawnNode, args=('3',))
+	thread4 = Thread(target=spawnNode, args=('4',))
+	thread5 = Thread(target=spawnNode, args=('5',))
+	thread6 = Thread(target=spawnNode, args=('7',))
+	thread7 = Thread(target=spawnNode, args=('6',))
+	thread8 = Thread(target=spawnNode, args=('8',))
+	thread0.start()
+	time.sleep(2)
+	thread1.start()
+	time.sleep(2)
+	thread2.start()
+	time.sleep(2)
+	thread3.start()
+	time.sleep(2)
+	thread4.start()
+	time.sleep(2)
+	thread5.start()
+	time.sleep(2)
+	thread6.start()
+	time.sleep(2)
+	thread7.start()
+	time.sleep(2)
+	thread8.start()
+
 
 
